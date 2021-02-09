f3a90e6b0c0352d005c607fcf974c20a11ef2952
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-02-09 13:32:24 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+// function divide(numOne, numTwo) {
+//     return numOne / numTwo
+// }
+
+let divide = () => 2000 / 100; 
+
+let square = num => num * num; 
+
+let add = (numOne, numTwo) => numOne + numTwo; 
